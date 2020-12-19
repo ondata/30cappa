158193df3b06e51a03e76076f29bc23f307b94c6
Andrea Borruso <aborruso@gmail.com>
2020-12-19 14:33:06 +0100

    corretto filtro su dati ISTAT

--- script/dataETL.sh
+++ script/dataETL.sh
@@ -12,24 +12,19 @@ mkdir -p "$folder"/../dati
 tail <"$folder"/../dati/rawdata/comuni.csv -n +2 >"$folder"/../dati/comuni.csv
 
 # crea totale popolazione per codice comunale
-mlr -I --csv cut -x -f Età \
-  then put '$Totale=${Totale Maschi}+${Totale Femmine}' \
-  then cut -x -f Età \
-  then stats1 -a sum -f Totale -g "Codice comune" \
-  then clean-whitespace then rename "Codice comune",PRO_COM_T,Totale_sum,Abitanti "$folder"/../dati/comuni.csv
+mlr -I --csv clean-whitespace \
+  then filter -S '${Età}=="999"' \
+  then put '$Abitanti=${Totale Maschi}+${Totale Femmine}' \
+  then cut -f "Codice comune",Abitanti \
+  then rename "Codice comune",PRO_COM_T "$folder"/../dati/comuni.csv
 
 # applica zero padding ai codici comunali, da 1001 a 001001
 mlr -I --csv put '$PRO_COM_T=fmtnum($PRO_COM_T,"%06d")' "$folder"/../dati/comuni.csv
 
-# estrai comuni <= 5000 abitanti
-#mlr -I --csv filter '$Abitanti<=5000' "$folder"/../dati/comuni.csv
-
 ### dati geografici ###
 
 # pulizia topologica di base
-#mapshaper "$folder"/../dati/rawdata/Limiti01012020_g/Com01012020_g/Com01012020_g_WGS84.shp -clean gap-fill-area=0 -filter 'COD_REG==19' -o "$folder"/../dati/tmp.shp
 mapshaper "$folder"/../dati/rawdata/Limiti01012020_g/Com01012020_g/Com01012020_g_WGS84.shp -clean gap-fill-area=0 -o "$folder"/../dati/tmp.shp
 
 # JOIN con CSV per abitanti
 mapshaper "$folder"/../dati/tmp.shp -join "$folder"/../dati/comuni.csv keys=PRO_COM_T,PRO_COM_T field-types=PRO_COM_T:str -o "$folder"/../dati/comuni.shp
-
