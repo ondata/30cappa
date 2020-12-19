c4ff05e884ebaf7239d533ee87cb878b47ee659f
pigr3co <pigrecoinfinito@gmail.com>
2020-12-19 22:41:20 +0100

    add script sql

---
+++ script/QGIS/script.sql
@@ -0,0 +1,41 @@
+--
+-- the present SQL script is intended to be executed from SpatiaLite_gui
+--
+-- initializing the output db-file
+--
+SELECT InitSpatialMetadata(1);
+
+-- crea un db vuoto e importa il vettore Com01012020_g_WGS84 e il CSV comuni
+-- in OUTPUT crea un vettore diff_buffer_capol
+
+-- crea vista - buffer da 30km su comuni da 5k
+CREATE VIEW v_comuni5k_ab_b30k AS
+SELECT c.pro_com_t,c.cod_reg,c.comune,p.Abitanti, st_buffer (geom,30000) as geom  FROM Com01012020_g_WGS84 c 
+JOIN (SELECT Abitanti, PRO_COM_T FROM comuni ) p ON c.pro_com_t=p.PRO_COM_T 
+WHERE p.Abitanti <=5000;
+
+INSERT INTO views_geometry_columns
+(view_name, view_geometry, view_rowid, f_table_name, f_geometry_column, read_only)
+VALUES ('v_comuni5k_ab_b30k', 'geom', 'rowid', 'com01012020_g_wgs84', 'geom',1);
+
+-- crea vista - capoluoghi di provincia
+CREATE VIEW v_capoluoghi_prov AS
+SELECT pro_com_t,cod_rip,cod_reg,comune,geom  
+FROM Com01012020_g_WGS84
+WHERE cc_uts != 0 ;
+
+INSERT INTO views_geometry_columns
+(view_name, view_geometry, view_rowid, f_table_name, f_geometry_column, read_only)
+VALUES ('v_capoluoghi_prov', 'geom', 'rowid', 'com01012020_g_wgs84', 'geom',1);
+
+-- clippa i capoluoghi con i buffer da 30km
+CREATE TABLE diff_buffer_capol AS
+SELECT b.pro_com_t,b.cod_reg,b.comune,CastToMulti(ST_DIFFERENCE(b.geom , p.geom)) as geom
+FROM v_comuni5k_ab_b30k b, (SELECT ST_UNION(GEOM) AS geom FROM v_capoluoghi_prov WHERE cod_reg=19 GROUP BY cod_reg) p
+WHERE b.cod_reg = 19 and ST_intersects (b.geom , p.geom) = 1;
+SELECT RecoverGeometryColumn('diff_buffer_capol','geom',32632,'MULTIPOLYGON','XY');
+
+--
+-- vacuuming the output db-file
+--
+VACUUM;
