6162f0aecb71c9b67e6c7b0dfd87efb44611ab4f
Andrea Borruso <aborruso@gmail.com>
2020-12-19 13:07:29 +0100

    corretto nome campi

--- script/dataETL.sh
+++ script/dataETL.sh
@@ -11,8 +11,11 @@ mkdir -p "$folder"/../dati
 # rimuovi riga intestazione
 tail <"$folder"/../dati/rawdata/comuni.csv -n +2 >"$folder"/../dati/comuni.csv
 
+# crea totale popolazione per codice comunale
 mlr -I --csv cut -x -f Età \
   then put '$Totale=${Totale Maschi}+${Totale Femmine}' \
   then cut -x -f Età \
   then stats1 -a sum -f Totale -g "Codice comune" \
-  then clean-whitespace "$folder"/../dati/comuni.csv
+  then clean-whitespace then rename "Codice comune",PRO_COM_T,Totale_sum,Abitanti "$folder"/../dati/comuni.csv
+
+# applica zero padding ai codici comunali
