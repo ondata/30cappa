8f91cdeea109de70b611c3964344cb1847ac3f67
Andrea Borruso <aborruso@gmail.com>
2020-12-19 15:06:14 +0100

    vedi #4

--- script/arigadicomando/downloadData.sh
+++ script/arigadicomando/downloadData.sh
@@ -4,24 +4,24 @@ set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir -p "$folder"/../dati/rawdata
+mkdir -p "$folder"/../../dati/rawdata
 
 # confini amministrativi versione generalizzata #
 
 URL_geo="https://www.istat.it/storage/cartografia/confini_amministrativi/generalizzati/Limiti01012020_g.zip"
 
 # scarica i dati
-curl -kL "$URL_geo" >"$folder"/../dati/rawdata/Limiti01012020_g.zip
+curl -kL "$URL_geo" >"$folder"/../../dati/rawdata/Limiti01012020_g.zip
 
 # decomprimi dati
-unzip "$folder"/../dati/rawdata/Limiti01012020_g.zip -d "$folder"/../dati/rawdata
+unzip "$folder"/../../dati/rawdata/Limiti01012020_g.zip -d "$folder"/../../dati/rawdata
 
 # popolazione residente #
 
 URL_pop="http://demo.istat.it/pop2020/dati/comuni.zip"
 
 # scarica i dati
-curl -kL "$URL_pop" >"$folder"/../dati/rawdata/comuni.zip
+curl -kL "$URL_pop" >"$folder"/../../dati/rawdata/comuni.zip
 
 # decomprimi dati
-unzip "$folder"/../dati/rawdata/comuni.zip -d "$folder"/../dati/rawdata
+unzip "$folder"/../../dati/rawdata/comuni.zip -d "$folder"/../../dati/rawdata
