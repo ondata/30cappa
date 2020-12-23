143a8a7d0ff0f2f7a117f27024c8e54333b6546c
pigr3co <pigrecoinfinito@gmail.com>
2020-12-23 17:41:27 +0100

    update cartella QGIS

--- script/QGIS/script.sql
+++ script/QGIS/script.sql
@@ -1,62 +1,73 @@
 --
 -- the present SQL script is intended to be executed from SpatiaLite_gui
 --
--- initializing the output db-file
---
 SELECT InitSpatialMetadata(1);
-
--- crea un db vuoto e importa il vettore Com01012020_g_WGS84 e il CSV comuni
--- in OUTPUT crea un vettore diff_buffer_capol
-
+--
+-- il vettore Com01012020_g_WGS84 contiene già il campo Abitanti demoistat2020
+-- in OUTPUT crea un vettore aree30cappa
+--
 -- crea tabella - buffer da 30km su comuni da 5k
-CREATE TABLE comuni5k_ab_b30k AS
-SELECT c.pro_com_t,c.cod_reg,c.comune,p.Abitanti, st_buffer (geom,30000) as geom  FROM Com01012020_g_WGS84 c 
-JOIN (SELECT Abitanti, PRO_COM_T FROM comuni ) p ON c.pro_com_t=p.PRO_COM_T 
-WHERE p.Abitanti <=5000;
-SELECT RecoverGeometryColumn('comuni5k_ab_b30k','geom',32632,'POLYGON','XY');
-SELECT CreateSpatialIndex('comuni5k_ab_b30k', 'geom');
-
--- crea vista - capoluoghi di provincia
+SELECT DropGeoTable( "b30k_comuni5k");
+CREATE TABLE b30k_comuni5k AS
+SELECT pro_com_t,cod_reg,comune,Abitanti, st_buffer (geom,30000) as geom  
+FROM Com01012020_g_WGS84 
+WHERE abitanti <=5000;
+SELECT RecoverGeometryColumn('b30k_comuni5k','geom',32632,'POLYGON','XY');
+SELECT CreateSpatialIndex('b30k_comuni5k', 'geom');
+--
+-- crea tabella - capoluoghi di provincia
+SELECT DropGeoTable( "capoluoghi_prov");
 CREATE TABLE capoluoghi_prov AS
 SELECT pro_com_t,cod_rip,cod_reg,comune,geom  
 FROM Com01012020_g_WGS84
 WHERE cc_uts != 0;
 SELECT RecoverGeometryColumn('capoluoghi_prov','geom',32632,'MULTIPOLYGON','XY');
 SELECT CreateSpatialIndex('capoluoghi_prov', 'geom');
-
+--
+-- crea tabella temporanea unendo i capoluoghi di provincia
+SELECT DropGeoTable( "tmp_cap_prov");
 CREATE TABLE tmp_cap_prov AS
 SELECT CastToMultiPolygon(ST_UNION(geom)) AS geom FROM capoluoghi_prov;
 SELECT RecoverGeometryColumn('tmp_cap_prov','geom',32632,'MULTIPOLYGON','XY');
-
-
--- clippa i capoluoghi con i buffer da 30km - cod_reg=19 Sicilia
-CREATE TABLE diff_buffer_capoluoghi AS
+--
+-- crea tabella temporanea unendo tutti i comuni
+SELECT DropGeoTable( "tmp_italia");
+CREATE TABLE tmp_italia AS
+SELECT CastToMultiPolygon(ST_UNION(geom)) AS geom FROM Com01012020_g_WGS84;
+SELECT RecoverGeometryColumn('tmp_italia','geom',32632,'MULTIPOLYGON','XY');
+--
+-- clippa italia con i buffer da 30km e comuni 5k
+SELECT DropGeoTable( "b30k_comuni5k_italia");
+CREATE TABLE b30k_comuni5k_italia AS
+SELECT b.pro_com_t,b.cod_reg,b.comune,CastToMultiPolygon(ST_Intersection(b.geom ,p.geom)) as geom
+FROM b30k_comuni5k b, tmp_italia p
+WHERE ST_intersects (b.geom , p.geom) = 1;
+SELECT RecoverGeometryColumn('b30k_comuni5k_italia','geom',32632,'MULTIPOLYGON','XY');
+--
+-- clippa i capoluoghi con i buffer da 30km e comuni 5k clippati con italia
+SELECT DropGeoTable( "aree30cappa");
+CREATE TABLE aree30cappa AS
 SELECT b.pro_com_t,b.cod_reg,b.comune,CastToMultiPolygon(ST_DIFFERENCE(b.geom , p.geom)) as geom
-FROM (SELECT * FROM comuni5k_ab_b30k)b, tmp_cap_prov p
+FROM b30k_comuni5k_italia b, tmp_cap_prov p
 WHERE ST_intersects (b.geom , p.geom) = 1
 union
 SELECT b.pro_com_t,b.cod_reg,b.comune,CastToMultiPolygon(b.geom) as geom
-FROM (SELECT * FROM comuni5k_ab_b30k)b,tmp_cap_prov p
+FROM b30k_comuni5k_italia b,tmp_cap_prov p
 WHERE ST_intersects (b.geom , p.geom) = 0;
-SELECT RecoverGeometryColumn('diff_buffer_capoluoghi','geom',32632,'MULTIPOLYGON','XY');
-SELECT CreateSpatialIndex('diff_buffer_capoluoghi', 'geom');
-
--- clippa i buffer comunali con la regione - da rivedere
-CREATE TABLE aree30cappa AS
-SELECT b.pro_com_t,b.cod_reg,p.cod_reg AS cod_reg_2,b.comune,CastToMultiPolygon(ST_Intersection(b.geom ,p.geom)) as geom
-FROM diff_buffer_capoluoghi b, tmp_regioni p
-WHERE ST_intersects (b.geom , p.geom) = 1
-AND
-b.ROWID IN (
-    SELECT ROWID 
-    FROM SpatialIndex
-    WHERE f_table_name = 'diff_buffer_capoluoghi' 
-        AND search_frame = p.geom)
-AND COD_REG = COD_REG_2;
 SELECT RecoverGeometryColumn('aree30cappa','geom',32632,'MULTIPOLYGON','XY');
-
-
+SELECT CreateSpatialIndex('aree30cappa', 'geom');
 --
--- vacuuming the output db-file
+-- cancella geotabelle inutili
+DROP TABLE IF EXISTS tmp_cap_prov;
+DROP TABLE IF EXISTS tmp_italia;
+DROP TABLE IF EXISTS b30k_comuni5k;
+DROP TABLE IF EXISTS capoluoghi_prov;
+DROP TABLE IF EXISTS tmp_cap_prov;
+DROP TABLE IF EXISTS tmp_italia;
+DROP TABLE IF EXISTS b30k_comuni5k_italia;
 --
+-- aggiorno statistiche e VACUUM (nel mio vecchio laptop impiega circa 7 minuti)
+--
+UPDATE geometry_columns_statistics set last_verified = 0;
+SELECT UpdateLayerStatistics('geometry_table_name');
 VACUUM;
