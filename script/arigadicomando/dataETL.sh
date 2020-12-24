39c5cbc87bf0fb887d39a78c08ee990904e4e1f8
Andrea Borruso <aborruso@gmail.com>
2020-12-24 15:17:45 +0100

    pulizia

--- script/arigadicomando/dataETL.sh
+++ script/arigadicomando/dataETL.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+### requisiti ###
+# gdal/ogr https://gdal.org/
+# mapshaper https://github.com/mbloch/mapshaper
+# gnu parallel https://www.gnu.org/software/parallel/
+# miller https://github.com/johnkerl/miller
+### requisiti ###
+
 set -x
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -7,7 +14,6 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$folder"/../../dati
 mkdir -p "$folder"/../../dati/arigadicomando
 mkdir -p "$folder"/../../dati/arigadicomando/output
-mkdir -p "$folder"/../../dati/arigadicomando/risorse
 
 ### dati sulla popolazione ###
 
@@ -35,10 +41,10 @@ mapshaper "$folder"/../../dati/tmp.shp -join "$folder"/../../dati/comuni.csv key
 # genera buffer a 30 kilometri dei comuni con meno di 5001 abitanti
 ogr2ogr -t_srs EPSG:4326 "$folder"/../../dati/arigadicomando/comuni_30cappa_5mila.shp "$folder"/../../dati/arigadicomando/comuni.shp -dialect sqlite -sql "SELECT PRO_COM_T,COD_REG,COMUNE,Abitanti,st_buffer(comuni.geometry,30000) AS geom FROM comuni where Abitanti <= 5000"
 
-# estrarre capoluoghi di provincia
+# estrai capoluoghi di provincia
 mapshaper "$folder"/../../dati/arigadicomando/comuni.shp -filter 'CC_UTS==1' -filter-fields PRO_COM_T,COD_REG,COMUNE,Abitanti -proj wgs84 -o "$folder"/../../dati/arigadicomando/capoluoghi_4326.shp
 
-# crea Italia
+# crea poligono stato Italia
 mapshaper "$folder"/../../dati/arigadicomando/comuni.shp -dissolve -proj wgs84 -o precision=0.000001 "$folder"/../../dati/arigadicomando/italia.shp
 
 # ritagliare i buffer dei comuni con il limite dell'italia
@@ -52,10 +58,7 @@ mlr -I --csv clean-whitespace "$folder"/../../dati/arigadicomando/comuni_30cappa
 mlr --c2t cut -f PRO_COM_T,COD_REG "$folder"/../../dati/arigadicomando/comuni_30cappa_5mila.CSV | tail -n +2 >"$folder"/../../dati/arigadicomando/lista.tsv
 
 ## estrai un geojson per ogni comune
-rm "$folder"/../../dati/arigadicomando/output_noreg/*json
 mapshaper "$folder"/../../dati/arigadicomando/tmp.shp -split PRO_COM_T -o format=geojson "$folder"/../../dati/arigadicomando/output_noreg/
 
 # rimuovi dai "poligoni buffer comune" area dei capoluoghi
 parallel --colsep "\t" -j100% 'mapshaper ../../dati/arigadicomando/output_noreg/{1}.json -erase ../../dati/arigadicomando/capoluoghi_4326.shp -o precision=0.000001 ../../dati/arigadicomando/output_noreg/{1}.geojson' :::: ./../../dati/arigadicomando/lista.tsv
-
-
