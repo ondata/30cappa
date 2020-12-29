d7c5a35b94195387f5d7c8d1814ac48470aaa679
pigr3co <pigrecoinfinito@gmail.com>
2020-12-29 17:35:45 +0100

    update scriptSQL

--- script/QGIS/scriptSQL/script_ottimizzato.sql
+++ script/QGIS/scriptSQL/script_ottimizzato.sql
@@ -22,7 +22,7 @@ FROM (SELECT pk_uid,pro_com_t, geom
 SELECT CreateSpatialIndex('b30k_comuni5k', 'geom');
 --
 -- crea tabella - capoluoghi di provincia
-SELECT DropGeoTable( "capoluoghi_prov");
+DROP TABLE IF EXISTS "capoluoghi_prov";
 CREATE TABLE "capoluoghi_prov" 
       ("pk_uid" integer PRIMARY KEY autoincrement NOT NULL,
        "pro_com_t" text);
@@ -37,7 +37,7 @@ FROM (SELECT pk_uid,pro_com_t,geom
       WHERE cc_uts != 0);
 --
 -- crea tabella temporanea unendo tutti i comuni
-SELECT DropGeoTable( "tmp_italia");
+DROP TABLE IF EXISTS "tmp_italia";
 CREATE TABLE "tmp_italia" 
       ("pk_uid" INTEGER PRIMARY KEY);
 -- aggiunge campo geom
@@ -49,7 +49,7 @@ UPDATE "tmp_italia" SET geom =
 (SELECT CastToMultiPolygon(ST_UNION(geom)) AS geom FROM "Com01012020_g_WGS84") WHERE  pk_uid = 1;
 --
 -- suddivide l'intero poligono nazionale
-SELECT DropGeoTable( "italia_subd2048");
+DROP TABLE IF EXISTS "italia_subd2048";
 CREATE TABLE "italia_subd2048" AS
 SELECT ST_Subdivide(geom,2048) AS geom FROM "tmp_italia";
 SELECT RecoverGeometryColumn('italia_subd2048','geom',32632,'MULTIPOLYGON','XY');
@@ -65,7 +65,7 @@ SELECT ElementaryGeometries( 'italia_subd2048',
 SELECT ST_Cutter(NULL, 'b30k_comuni5k', NULL, NULL, 'italia_subd2048_elem', NULL, 'b30k_com5k_italy_subd_elem', 1, 1);
 --
 -- ricompone i pezzi - ricorda che pk_id si riferisce alla tabella comuni 'Com01012020_g_WGS84'
-SELECT DropGeoTable( "b30k_com5k_italy");
+DROP TABLE IF EXISTS "b30k_com5k_italy";
 CREATE TABLE "b30k_com5k_italy" 
       ("pk_uid" INTEGER PRIMARY KEY);
 -- aggiunge campo geom
@@ -83,7 +83,7 @@ SELECT RecoverGeometryColumn('b30k_com5k_italy','geom',32632,'MULTIPOLYGON','XY'
 SELECT ST_Cutter(NULL, 'b30k_com5k_italy', NULL, NULL, 'capoluoghi_prov', NULL, 'b30k_com5k_italy_subd_elem_capolp', 1, 1);
 --
 -- ricompone i pezzi - ricorda che pk_uid si riferisce alla tabella comuni 'Com01012020_g_WGS84'
-SELECT DropGeoTable( "finale");
+DROP TABLE IF EXISTS "finale";
 CREATE TABLE "finale" 
       ("pk_uid" INTEGER PRIMARY KEY);
 -- aggiunge campo geom
@@ -93,12 +93,12 @@ INSERT INTO "finale"
 SELECT k."input_b30k_com5k_italy_pk_uid" AS pk_uid, k.geom
 FROM (SELECT "input_b30k_com5k_italy_pk_uid", CastToMultiPolygon(st_union("geom")) AS geom
       FROM "b30k_com5k_italy_subd_elem_capolp"
-      WHERE "blade_capoluoghi_prov_pk_uid" is null
+      WHERE "blade_capoluoghi_prov_pk_uid" IS NULL AND ST_Area(geom) > 1
       group by "input_b30k_com5k_italy_pk_uid") k;
 SELECT RecoverGeometryColumn('finale','geom',32632,'MULTIPOLYGON','XY');
 --
 -- associa campo pro_com_t ed altri
-SELECT DropGeoTable( "aree30cappa");
+DROP TABLE IF EXISTS "aree30cappa";
 CREATE TABLE "aree30cappa" AS
 SELECT f."pk_uid",c."pro_com_t",c."cod_reg",c."comune",c."abitanti",f."geom"
 FROM "finale" f left join "Com01012020_g_WGS84" c USING ("pk_uid");
